ac2ce84ffcabadce7d3c6e2c796236836c292805
GaoBin <bin_gao@live.cn>
2012-06-11 11:37:16 +0800

    add a test for helloworld

---
+++ js/HelloWorld.js
@@ -0,0 +1,43 @@
+var smr = smr || {};
+
+(function($){
+
+	// --------- Component Interface Implementation ---------- //
+	function HelloWorld(){};
+	smr.HelloWorld = HelloWorld; 
+  
+	HelloWorld.prototype.build = function(data,config){
+		var html = $("#tmpl-HelloWorld").render({});
+		return $(html);
+	}
+		
+	HelloWorld.prototype.postDisplay = function(pdata, config) {
+		var $e = this.$element;
+		var thisC = this;
+		
+		 d3.select(".HelloWorld")
+	      .style("width", "0%")
+	      .style("background-color", "steelblue")
+	      .text("hello\u00a0world")
+	    .transition()
+	      .ease("bounce")
+	      .duration(2000)
+	      .style("width", "100%")
+	      .style("background-color", "brown");
+		 
+		 window.addEventListener("keypress", transform, false);
+	}
+	// --------- /Component Interface Implementation ---------- //
+	
+	
+	// --------- Component Registration --------- //
+	brite.registerComponent("HelloWorld",{
+		emptyParent: true,
+		loadTmpl: true
+	},
+	function(){
+		return new smr.HelloWorld();
+	});	
+	// --------- /Component Registration --------- //
+	
+})(jQuery);
